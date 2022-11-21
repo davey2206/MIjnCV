e2a0b17110cb3b2621da99598c80a5b6cf4007e6
davey2206 <davey2206@live.nl>
2022-11-21 11:57:53 +0100

    added section support

---
+++ mijncv/src/components/LayoutMainAlt.vue
@@ -0,0 +1,58 @@
+<style scoped>
+#section{
+    padding-top: 52px;
+    width: 95vw;
+    height: 95vh;
+    margin: 1em;
+}
+#Image{
+    width: 80%;
+}
+.row{
+    height: 100%;
+    width: 100%;
+}
+</style>
+<template>
+    <div id="section">
+        <section v-if="loading">
+            <h1> loading.....</h1>
+        </section>
+        <section v-else>
+            {{Test}}
+            <div class="row align-items-center">
+                <div class="col">
+                    <img id="Image" v-bind:src="Pic">
+                </div>
+                <div class="col">
+                    <div class="container">
+                        <h1>{{ Test.title }}</h1>
+                        <p>
+                            {{ Test.paragraph }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    props: {
+        ID: null,
+        Pic: String
+    },
+    data () {
+        return {
+        Test: null,
+        loading: true
+        }
+    },
+    mounted(){
+        axios.get("https://localhost:7059/api/Sections/" + this.ID).then(response => (this.Test = response.data)).finally(() => this.loading = false)
+    }
+}
+</script>
--- mijncv/src/views/TestView.vue
+++ mijncv/src/views/TestView.vue
@@ -5,7 +5,14 @@
     </section>
     <section v-else>
       <NavBar></NavBar>
-      <LayoutMain ID="7" Pic="https://cdn.discordapp.com/attachments/438780813807714304/1039513899537547264/unknown.png"></LayoutMain>
+      <div v-for="Test in Tests">
+        <div v-if="Test.layout == 1">
+          <LayoutMain :ID="Test.id" Pic="https://cdn.discordapp.com/attachments/438780813807714304/1039513899537547264/unknown.png"></LayoutMain>
+        </div>
+        <div v-if="Test.layout == 2">
+          <LayoutMainAlt :ID="Test.id" Pic="https://cdn.discordapp.com/attachments/438780813807714304/1039513899537547264/unknown.png"></LayoutMainAlt>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -14,22 +21,24 @@
 // @ is an alias to /src
 import NavBar from '@/components/NavBar.vue';
 import LayoutMain from '@/components/LayoutMain.vue';
+import LayoutMainAlt from '@/components/LayoutMainAlt.vue';
 import axios from 'axios';
 
 export default {
   name: 'HomeView',
   components: {
     NavBar,
-    LayoutMain
+    LayoutMain,
+    LayoutMainAlt
   },
   data () {
         return {
-        Test: null,
+        Tests: null,
         loading: true
         }
     },
     mounted(){
-        axios.get("https://localhost:7059/api/Sections").then(response => (this.Test = response.data)).finally(() => this.loading = false)
+        axios.get("https://localhost:7059/api/Sections").then(response => (this.Tests = response.data)).finally(() => this.loading = false)
     }
 }
 </script>
